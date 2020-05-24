ff2b91ee2d7a29ceef9a541ebfa51e1ad0b6a9be
gtm417 <godik417@gmail.com>
2020-05-24 16:12:07 +0300

    add WebServiceConfiguration

---
+++ src/main/java/com/example/configuration/WebServiceConfiguration.java
@@ -0,0 +1,36 @@
+package com.example.configuration;
+
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.ws.config.annotation.WsConfigurerAdapter;
+import org.springframework.ws.transport.http.MessageDispatcherServlet;
+import org.springframework.ws.wsdl.wsdl11.DefaultWsdl11Definition;
+import org.springframework.xml.xsd.SimpleXsdSchema;
+import org.springframework.xml.xsd.XsdSchema;
+
+public class WebServiceConfiguration extends WsConfigurerAdapter {
+    @Bean
+    public ServletRegistrationBean messageDispatcherServlet(ApplicationContext applicationContext) {
+        MessageDispatcherServlet servlet = new MessageDispatcherServlet();
+        servlet.setApplicationContext(applicationContext);
+        servlet.setTransformWsdlLocations(true);
+        return new ServletRegistrationBean(servlet, "/ws/*");
+    }
+
+    @Bean(name = "doctors")
+    public DefaultWsdl11Definition defaultWsdl11Definition(XsdSchema coursesSchema) {
+        DefaultWsdl11Definition wsdl11Definition = new DefaultWsdl11Definition();
+        wsdl11Definition.setPortTypeName("Doctors");
+        wsdl11Definition.setLocationUri("/ws");
+        wsdl11Definition.setTargetNamespace("http://soapService.tutorial");
+        wsdl11Definition.setSchema(coursesSchema);
+        return wsdl11Definition;
+    }
+
+    @Bean
+    public XsdSchema coursesSchema() {
+        return new SimpleXsdSchema(new ClassPathResource("doctors.xsd"));
+    }
+}
